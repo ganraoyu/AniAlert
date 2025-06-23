34db4c1b0866b501e155dc11fc6255cf0b01a0d4
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2025-06-23 13:26:29 -0400

    Feature: Add MyAnimeList API provider module

---
+++ AniAlert/providers/my_anime_list.py
@@ -0,0 +1,34 @@
+import requests 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id = os.getenv('CLIENT_ID')
+
+headers = {
+    'X-MAL-CLIENT-ID': client_id,
+}
+
+response = requests.get(
+    'https://api.myanimelist.net/v2/anime?q=naruto&limit=1',
+    headers=headers
+) 
+
+def search_anime(query, limit):
+  response = requests.get(
+    f'https://api.myanimelist.net/v2/anime?q={query}&limit={limit}',
+    headers=headers
+  )
+
+  data = response.json()
+  found_animes = []
+
+  for anime in data['data']:
+   title = anime['node']['title']
+   found_animes.append(title)
+
+  return found_animes, data
+
+my_animes = search_anime('Bleach', 10)
+print(my_animes)
